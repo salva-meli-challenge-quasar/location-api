4d90b23bc149c99490bdfabdb43f016f3e81ba8c
ricardo <rsalva@devartis.com>
2021-01-18 12:56:59 -0300

    Adding boilerplate

---
+++ src/main/java/com/location/api/controller/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.location.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+	@GetMapping("/welcome")
+	public String welcome() {
+		return "this is a test";
+	}
+}
