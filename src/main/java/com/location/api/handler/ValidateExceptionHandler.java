e7d196ef38e4dd46a8c5c1e9091fb8183b7e68e6
ricardo <rsalva@devartis.com>
2021-01-19 12:09:52 -0300

    Fixing typo

--- src/main/java/com/location/api/handler/ValidateExceptionHandler.java
+++ src/main/java/com/location/api/handler/ValidateExceptionHandler.java
@@ -17,7 +17,7 @@ public class ValidateExceptionHandler {
 	@ExceptionHandler(MethodArgumentNotValidException.class)
 	public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
 		Map<String, String> errors = new HashMap<>();
-	    ex.getBindingResult().getAllErrors().forEach((error) -> {
+	    ex.getBindingResult().getAllErrors().forEach(error -> {
 	        String fieldName = ((FieldError) error).getField();
 	        String errorMessage = error.getDefaultMessage();
 	        errors.put(fieldName, errorMessage);
