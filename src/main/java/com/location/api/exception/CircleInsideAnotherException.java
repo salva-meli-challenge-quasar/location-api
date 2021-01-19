fcf7340623b8cbf402a6b47b4ae54029b0798f26
ricardo <rsalva@devartis.com>
2021-01-18 21:41:08 -0300

    Fixing request status code in error

--- src/main/java/com/location/api/exception/CircleInsideAnotherException.java
+++ src/main/java/com/location/api/exception/CircleInsideAnotherException.java
@@ -1,9 +1,5 @@
 package com.location.api.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(code = HttpStatus.BAD_REQUEST)
 public class CircleInsideAnotherException extends TwoDimensionalTrilaterationException {
 
 	public CircleInsideAnotherException(String message) {
--- src/main/java/com/location/api/exception/TwoDimensionalTrilaterationException.java
+++ src/main/java/com/location/api/exception/TwoDimensionalTrilaterationException.java
@@ -3,7 +3,7 @@ package com.location.api.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.BAD_REQUEST)
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
 public class TwoDimensionalTrilaterationException extends Exception {
 
 	public TwoDimensionalTrilaterationException(String message) {
